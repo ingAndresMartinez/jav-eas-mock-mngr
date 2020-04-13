ad92542403cba732014c15bd275ca901cff752b9
ingAndresMartinez <ing.andresmartinezcobos@gmail.com>
2020-04-13 18:09:51 -0500

    Add java project

---
+++ src/main/java/co/edu/javeriana/eas/patterns/mocks/JavEasPatternsMockMngrApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.patterns.mocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavEasPatternsMockMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavEasPatternsMockMngrApplication.class, args);
+	}
+
+}
